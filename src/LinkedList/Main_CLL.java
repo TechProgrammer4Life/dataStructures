222ac24a1981b7dce4cfdcede9b4c7c12d840ebb
Muralikrishna <muralikrishna.in@outlook.com>
2025-02-20 21:25:26 -0500

    LinkedList

---
+++ src/LinkedList/Main_CLL.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+public class Main_CLL {
+
+    public static void main(String[] args) {
+
+        CLL list = new CLL();
+        list.insert(23);
+        list.insert(3);
+        list.insert(19);
+        list.insert(75);
+        list.display();
+        list.delete(19);
+        list.display();
+
+    }
+
+}
